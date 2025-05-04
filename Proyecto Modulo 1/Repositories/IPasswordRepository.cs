0e9aecaec23e8af8ca1914a14e420e69f47a74cd
Jose <urdaneta.j@outlook.com>
2025-05-04 17:55:43 -0500

    Fix

--- Proyecto Modulo 1/Repositories/IPasswordRepository.cs
+++ Proyecto Modulo 1/Repositories/IPasswordRepository.cs
@@ -10,6 +10,6 @@ namespace Proyecto_Modulo_1.Repositories
     public interface IPasswordRepository : IRepository<PasswordStorage>
     {
         List<PasswordStorage> BuscarPorServicio(string serviceName);
-        List<PasswordStorage> ObtenerPorUsuario(int userId);
+        List<PasswordStorage> ObtenerPorUsuario(int userId); 
     } 
 }
