b663c03db6440012b2309df01a2bb26413a46b9b
Jose <urdaneta.j@outlook.com>
2025-05-04 17:55:31 -0500

    Agregar Services

---
+++ Proyecto Modulo 1/Services/IPasswordStorageService.cs
@@ -0,0 +1,18 @@
+﻿using Proyecto_Modulo_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Modulo_1.Services
+{
+    public interface IPasswordStorageService
+    {
+        List<PasswordStorage> ObtenerTodas(int userId);
+        PasswordStorage ObtenerPorId(int id);
+        void Crear(PasswordStorage password);
+        void Actualizar(PasswordStorage password);
+        void Eliminar(PasswordStorage password);
+    }
+}
--- Proyecto Modulo 1/Services/PasswordStorageService.cs
+++ Proyecto Modulo 1/Services/PasswordStorageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Proyecto_Modulo_1.Models;
+using Proyecto_Modulo_1.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,45 @@ using System.Threading.Tasks;
 
 namespace Proyecto_Modulo_1.Services
 {
-    public class PasswordStorageService
+    public class PasswordStorageService : IPasswordStorageService
     {
+        private readonly IPasswordRepository _passwordRepository;
 
+        public PasswordStorageService(IPasswordRepository passwordRepository)
+        {
+            _passwordRepository = passwordRepository;
+        }
+
+        public List<PasswordStorage> ObtenerTodas(int userId)
+        {
+            return _passwordRepository.GetAll().Where(p => p.UserId == userId).ToList();
+        }
+
+        public PasswordStorage ObtenerPorId(int id)
+        {
+            return _passwordRepository.GetById(id);
+        }
+
+        public void Crear(PasswordStorage password)
+        {
+            if (string.IsNullOrWhiteSpace(password.ServiceName) ||
+                string.IsNullOrWhiteSpace(password.ServiceUsername) ||
+                string.IsNullOrWhiteSpace(password.ServicePassword))
+            {
+                throw new ArgumentException("Los campos no pueden estar vacíos.");
+            }
+
+            _passwordRepository.Add(password);
+        }
+
+        public void Actualizar(PasswordStorage password)
+        {
+            _passwordRepository.Update(password);
+        }
+
+        public void Eliminar(PasswordStorage password)
+        {
+            _passwordRepository.Delete(password);
+        }
     }
 }
