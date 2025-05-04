e27c0968bdec6613877abf6aec0b436ee3a9690b
Jose <urdaneta.j@outlook.com>
2025-05-04 16:47:48 -0500

    Creación de Models

---
+++ Proyecto Modulo 1/Models/PasswordStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Modulo_1.Models
+{
+    public class PasswordStorage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceUsername { get; set; }
+        public string ServicePassword { get; set; }
+
+    }
+}
